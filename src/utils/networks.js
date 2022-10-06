bf1bdd3ebd25416f3825cda10554d1263155910d
Elvolution <75731970+Elvolution20@users.noreply.github.com>
2022-10-06 18:46:50 +0100

    commit

---
+++ src/utils/networks.js
@@ -0,0 +1,18 @@
+/* prettier-ignore */
+const networks = {
+  "1": "Testnet",
+  "3": "Ropsten",
+  "2a": "Kovan",
+  "4": "Rinkeby",
+  "5": "Goerli",
+  "61": "BSC Testnet",
+  "38": "BSC Testnet",
+  "89": "Polygon Testnet",
+  "13881": "Polygon Mumbai Testnet",
+  "a86a": "AVAX Testnet",
+  "10D": "HPB Testnet",
+  "53": "CoinEx Testnet",
+
+}
+
+export { networks };
